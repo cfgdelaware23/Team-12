62dbdc0e78490e83152bd2a1ab584b836e855f3f
Mauricio Del Castillo <mauriciodelcas30@gmail.com>
2023-09-30 06:08:34 -0400

    updated

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,9 @@
-import './Home.css';
-import { Link } from 'react-router-dom';
-import Navbar from '../components/Navbar/navbar';
-import Calendar from '../components/calendar';
+import "./Home.css";
+import { Link } from "react-router-dom";
+import Navbar from "../components/Navbar/navbar";
+import Calendar from "../components/calendar";
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 function Home() {
   const [totalVolunteerHours, setTotalVolunteerHours] = useState(0);
@@ -11,9 +11,6 @@ function Home() {
   const [totalUsers, setTotalUsers] = useState(2);
   const [recommendedEvents, setRecommendedEvents] = useState([]);
   const [userPreferences, setUserPreferences] = useState([]);
-  
-  
-  
 
   const options = {
     method: "POST",
@@ -23,56 +20,56 @@ function Home() {
       "Content-Type": "application/json;charset=UTF-8",
     },
     body: JSON.stringify({
-      email: "abcd@gmail.com"
+      email: "abcd@gmail.com",
     }),
   };
 
   // get user preferences & find recommended events
   useEffect(() => {
-    fetch('http://localhost:3001/getUserPreferences', options)
-    //.then((response) => response.json())
-    .then((data) => {
-      // set user preferences
-      console.log(data.categories)
-      setUserPreferences(data.categories)
-      // post request for recommendation api
-      const options = {
-        method: "POST",
-        mode: "cors",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json;charset=UTF-8",
-        },
-        body: JSON.stringify({
-          categories: userPreferences
-        }),
-      };
-      // fetch recommendation api
-      fetch('http://localhost:3001/getUserPreferences', options)
-        .then((response) => response.json())
-        .then((data) => {
-          setRecommendedEvents(data.events)
-          console.log(recommendedEvents.length)
-          // generates random range of 10 events to be presented
-          // or if length is less than 10, then all recommended
-          // events are presented
-          if (recommendedEvents.length > 5){
-            var startingValue = Math.random(0, recommendedEvents.length-5)
-            setRecommendedEvents(recommendedEvents.slice(startingValue, startingValue + 5))
-          }
-      }).catch((error) => {
-        console.error(error)
-      })
-    })
-  })
+    fetch("http://localhost:3001/getUserPreferences", options)
+      //.then((response) => response.json())
+      .then((data) => {
+        // set user preferences
+        console.log(data.categories);
+        setUserPreferences(data.categories);
+        // post request for recommendation api
+        const options = {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json;charset=UTF-8",
+          },
+          body: JSON.stringify({
+            categories: userPreferences,
+          }),
+        };
+        // fetch recommendation api
+        fetch("http://localhost:3001/getUserPreferences", options)
+          .then((response) => response.json())
+          .then((data) => {
+            setRecommendedEvents(data.events);
+            console.log(recommendedEvents.length);
+            // generates random range of 10 events to be presented
+            // or if length is less than 10, then all recommended
+            // events are presented
+            if (recommendedEvents.length > 5) {
+              var startingValue = Math.random(0, recommendedEvents.length - 5);
+              setRecommendedEvents(
+                recommendedEvents.slice(startingValue, startingValue + 5)
+              );
+            }
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      });
+  });
   // get recommended user events
-  
 
-
-  
   useEffect(() => {
-    // Fetch the total volunteer hours 
-    fetch('/api/volunteer-hours')
+    // Fetch the total volunteer hours
+    fetch("/api/volunteer-hours")
       .then((response) => response.json())
       .then((data) => {
         setTotalVolunteerHours(data.totalHours);
@@ -82,7 +79,7 @@ function Home() {
       });
 
     // Fetch the total number of events
-    fetch('/api/total-events')
+    fetch("/api/total-events")
       .then((response) => response.json())
       .then((data) => {
         setTotalEvents(data.totalEvents);
@@ -91,8 +88,8 @@ function Home() {
         //console.error(error);
       });
 
-    // Fetch the total number of users 
-    fetch('/api/total-users')
+    // Fetch the total number of users
+    fetch("/api/total-users")
       .then((response) => response.json())
       .then((data) => {
         setTotalUsers(data.totalUsers);
@@ -100,31 +97,30 @@ function Home() {
       .catch((error) => {
         //console.error(error);
       });
-  }, [])
+  }, []);
   return (
-    <> 
-      <Navbar /> 
+    <>
+      <Navbar />
       <div className="dashboard">
-      <h1>Dashboard</h1>
-      <div class = "row">
-        <div className="dashboardCards">
-          <h2> Number of Events: {totalEvents}</h2>
-          <h3> {totalEvents} </h3>
+        <h1>Dashboard</h1>
+        <div class="row">
+          <div className="dashboardCards">
+            <h2> Number of Events: {totalEvents}</h2>
+            <h3> {totalEvents} </h3>
+          </div>
+          <div className="dashboardCards">
+            <h2> Number of Users: </h2>
+            <h3> {totalUsers} </h3>
+          </div>
+          <div className="dashboardCards">
+            <h2> Number of Volunteer Hours: </h2>
+            <h3> {totalVolunteerHours} </h3>
+          </div>
         </div>
-        <div className="dashboardCards">
-          <h2> Number of Users:  </h2>
-          <h3> {totalUsers} </h3>
-        </div>
-        <div className="dashboardCards">
-          <h2> Number of Volunteer Hours:  </h2>
-          <h3> {totalVolunteerHours} </h3>
-        </div>
-      </div>
       </div>
       <div class="calendar">
-        <Calendar /> 
+        <Calendar />
       </div>
-
     </>
   );
 }
