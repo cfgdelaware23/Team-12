2b841d10c45343a4b1a3e0a0c12a24e0e0952719
Dhanush Ananthkar <dhanush427@gmail.com>
2023-09-29 21:40:08 -0400

    profile first push

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,8 +1,73 @@
+import React, { useState } from 'react';
 import './Profile.css';
+//import { Calendar } from 'rsuite';
+//import 'rsuite/dist/rsuite-no-reset.min.css';
+
+
+import pic from './images/headshot.jpg';
 
 function Profile() {
+  const [showProfile, setShowProfile] = useState(true);
+
+  const handleTabClick = (tab) => {
+    if (tab === 'Profile') {
+      setShowProfile(true);
+    } else {
+      setShowProfile(false);
+    }
+  };
+
   return (
- <h1>CreateEvent Page</h1>
+    <div className="container">
+      <aside>
+        <div className="navbar">
+          <div className="logo">
+            <img src={pic} alt="Profile Pic" />
+          </div>
+          <div>
+            <span>Mary Ann</span>
+          </div>
+          <nav>
+            <ul>
+              <li
+                className={`select ${showProfile ? 'active' : ''}`}
+                name="home"
+                onClick={() => handleTabClick('Profile')}
+              >
+                Profile
+              </li>
+              <li
+                className={!showProfile ? 'active' : ''}
+                name="about"
+                onClick={() => handleTabClick('Calendar')}
+              >
+                Calendar
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </aside>
+
+      <main>
+        <div className={`card ${showProfile ? 'active' : ''}`} data-home>
+          <div className="title">Profile</div>
+          <div className="content">
+            <div>hi</div>
+            <div className="box">Address</div>
+            <div className="box">Phone Number</div>
+            <div className="box">Email</div>
+            <div className="box">Interests</div>
+            <div className="box">Member since ...</div>
+            <div className="box">Total Volunteering Hours: </div>
+          </div>
+        </div>
+
+        <div className={`card ${!showProfile ? 'active' : ''}`} data-about>
+          <div className="title">Calendar</div>
+          <div className="content">*Insert here*</div>
+        </div>
+      </main>
+    </div>
   );
 }
 
