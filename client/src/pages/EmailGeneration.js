c4d28a06dc0c44e890ac632030b985619194f39a
MsJoyA <ashleyjoyetheridge@gmail.com>
2023-09-30 00:53:21 -0400

    Email Generation Page

---
+++ client/src/pages/EmailGeneration.js
@@ -0,0 +1,11 @@
+import './CreateEvent.css';
+
+function EmailGeneration () {
+    return (
+        <div>
+            <h3> Email EmailGeneration</h3>
+
+        </div>
+    )
+}
+ export default EmailGeneration
