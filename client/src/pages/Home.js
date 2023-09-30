4353af0900c1cc02857bb5b1e2e9c68aeb2b4a28
Mauricio Del Castillo <mauriciodelcas30@gmail.com>
2023-09-30 06:13:18 -0400

    fixed bug

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,62 +9,62 @@ function Home() {
   const [totalVolunteerHours, setTotalVolunteerHours] = useState(0);
   const [totalEvents, setTotalEvents] = useState(50000);
   const [totalUsers, setTotalUsers] = useState(2);
-  const [recommendedEvents, setRecommendedEvents] = useState([]);
-  const [userPreferences, setUserPreferences] = useState([]);
+  // const [recommendedEvents, setRecommendedEvents] = useState([]);
+  // const [userPreferences, setUserPreferences] = useState([]);
 
-  const options = {
-    method: "POST",
-    mode: "cors",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json;charset=UTF-8",
-    },
-    body: JSON.stringify({
-      email: "abcd@gmail.com",
-    }),
-  };
+  // const options = {
+  //   method: "POST",
+  //   mode: "cors",
+  //   headers: {
+  //     Accept: "application/json",
+  //     "Content-Type": "application/json;charset=UTF-8",
+  //   },
+  //   body: JSON.stringify({
+  //     email: "abcd@gmail.com",
+  //   }),
+  // };
 
   // get user preferences & find recommended events
-  useEffect(() => {
-    fetch("http://localhost:3001/getUserPreferences", options)
-      //.then((response) => response.json())
-      .then((data) => {
-        // set user preferences
-        console.log(data.categories);
-        setUserPreferences(data.categories);
-        // post request for recommendation api
-        const options = {
-          method: "POST",
-          mode: "cors",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json;charset=UTF-8",
-          },
-          body: JSON.stringify({
-            categories: userPreferences,
-          }),
-        };
-        // fetch recommendation api
-        fetch("http://localhost:3001/getUserPreferences", options)
-          .then((response) => response.json())
-          .then((data) => {
-            setRecommendedEvents(data.events);
-            console.log(recommendedEvents.length);
-            // generates random range of 10 events to be presented
-            // or if length is less than 10, then all recommended
-            // events are presented
-            if (recommendedEvents.length > 5) {
-              var startingValue = Math.random(0, recommendedEvents.length - 5);
-              setRecommendedEvents(
-                recommendedEvents.slice(startingValue, startingValue + 5)
-              );
-            }
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-      });
-  });
+  // useEffect(() => {
+  //   fetch("http://localhost:3001/getUserPreferences", options)
+  //     //.then((response) => response.json())
+  //     .then((data) => {
+  //       // set user preferences
+  //       console.log(data.categories);
+  //       setUserPreferences(data.categories);
+  //       // post request for recommendation api
+  //       const options = {
+  //         method: "POST",
+  //         mode: "cors",
+  //         headers: {
+  //           Accept: "application/json",
+  //           "Content-Type": "application/json;charset=UTF-8",
+  //         },
+  //         body: JSON.stringify({
+  //           categories: userPreferences,
+  //         }),
+  //       };
+  //       // fetch recommendation api
+  //       fetch("http://localhost:3001/getUserPreferences", options)
+  //         .then((response) => response.json())
+  //         .then((data) => {
+  //           setRecommendedEvents(data.events);
+  //           console.log(recommendedEvents.length);
+  //           // generates random range of 10 events to be presented
+  //           // or if length is less than 10, then all recommended
+  //           // events are presented
+  //           if (recommendedEvents.length > 5) {
+  //             var startingValue = Math.random(0, recommendedEvents.length - 5);
+  //             setRecommendedEvents(
+  //               recommendedEvents.slice(startingValue, startingValue + 5)
+  //             );
+  //           }
+  //         })
+  //         .catch((error) => {
+  //           console.error(error);
+  //         });
+  //     });
+  // });
   // get recommended user events
 
   useEffect(() => {
