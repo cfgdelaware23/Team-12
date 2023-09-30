4610e33c1923f98b4fa36c489bd236477c30e058
emilygrsmith <emilygrsmith11@gmail.com>
2023-09-30 03:50:42 -0400

    skeleton

--- client/src/pages/VolunteerSignUp.js
+++ client/src/pages/VolunteerSignUp.js
@@ -1,18 +1,37 @@
-import './VolunteerSignUp.css';
-import Card from '../components/Card/Card'
+import React, { useState, useEffect } from 'react';
+import Card from '../components/Card/Card';
+
 function VolunteerSignUp() {
-  const submit = () =>
-  {
-      //post
-  }
+  const [cards, setCards] = useState([]);
+
+  // Use useEffect to fetch data when the component mounts
+  useEffect(() => {
+    getAllEvents();
+  }, []);
+
+  const getAllEvents = () => {
+    fetch('/api/getEmptyEvents')
+      .then((response) => response.json())
+      .then((data) => {
+        setCards(data); // Assuming the data is an array of cards
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
-      //get all models
-      //for models that apply
-      <div className = "options">
-    <Card name = 'call name'time = "time here"/>
-    <button onClick = {submit}>Register for this Event</button>
+    <div className="options">
+      {cards.map((event) => (
+        <Card key={event.id} event={event} />
+      ))}
     </div>
   );
 }
 
 export default VolunteerSignUp;
+
+
+
+
+
