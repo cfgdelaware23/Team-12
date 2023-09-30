6a98cdfa0a9b954a3dd81cda219c74f2e49c8a14
Toye Sobayo <sobayotoye482@gmail.com>
2023-09-30 07:29:17 -0400

    fixed email list

--- server/server.js
+++ server/server.js
@@ -198,12 +198,14 @@ app.post("/volunteer", async (req, res) => {
 // email list
 app.post("/emaillist", async (req, res) => {
   // email object that contains the email as well as params for sending
-  const emailBody = { emailAddress, email };
+  const { emailAddress, email } = req.body;
 
   // check if the incoming data is even an email
-  if (!email) {
+  if (!emailAddress) {
     return res.status(400).json({ "Error: Not an email "})
   } 
+
+  const emailBody = { emailAddress, email };
 
   // return email body if it's an email
   return res.status(200).json(emailBody)
