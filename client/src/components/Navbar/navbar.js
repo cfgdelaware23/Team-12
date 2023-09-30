3730ed4cf490c972e5935ff4f87b21e44703b0c0
nataliechow <nchow234@gmail.com>
2023-09-30 02:29:41 -0400

    async function to fetch event data from backend

--- client/src/components/Navbar/navbar.js
+++ client/src/components/Navbar/navbar.js
@@ -15,6 +15,7 @@ return (
 <Link to="/EventData" onClick = {() => {window.location.href="/EventData"}} > Event Data </Link>
 <Link to="/VolunteerSignUp" onClick = {() => {window.location.href="/VolunteerSignUp"}} > Volunteer Sign Up </Link>
 <Link to="/Profile" onClick = {() => {window.location.href="/Profile"}} > Profile </Link>
+
 </div>
 </div>
 </header>
