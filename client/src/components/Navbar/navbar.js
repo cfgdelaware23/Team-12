0b070554f5c7a6f0e9689e4ad61030d808106415
emilygrsmith <emilygrsmith11@gmail.com>
2023-09-29 22:08:30 -0400

    merge fix

--- client/src/components/Navbar/navbar.js
+++ client/src/components/Navbar/navbar.js
@@ -3,25 +3,6 @@ import './navbar.css'
 
 
 function navbar() {
-<<<<<<< HEAD
-    return (
-    <header>
-    
-    <div className="sidebar">
-    <p> Visionary Advocates </p>
-              <Link to="/" onClick = {() => {window.location.href="/"}} > Home </Link>
-              <Link to="/Login" onClick = {() => {window.location.href="/Login"}} > Login </Link>
-              <Link to="SignUp" onClick = {() => {window.location.href="/SignUp"}} > Sign Up </Link>
-              <Link to="/CreateEvent" onClick = {() => {window.location.href="/CreateEvent"}} > Create Event </Link>
-              <Link to="/VolunteerSignUp" onClick = {() => {window.location.href="/VolunteerSignUp"}} > Volunteer Sign Up </Link>
-              <Link to="/Profile" onClick = {() => {window.location.href="/Profile"}} > Profile </Link>
-      </div>
-    </header>
-    )
-  }
-  
-  export default navbar
-=======
 return (
 <header>
 <div className="sidebar">
@@ -37,4 +18,3 @@ return (
 )
 }
 export default navbar
->>>>>>> profile
