f0ca2e790c3358c5c7da43a06bcc76580121832d
Dhanush Ananthkar <dhanush427@gmail.com>
2023-09-30 00:43:27 -0400

    fixed navbar

--- client/src/components/Navbar/navbar.js
+++ client/src/components/Navbar/navbar.js
@@ -7,12 +7,14 @@ return (
 <header>
 <div className="sidebar">
 <p> Visionary Advocates </p>
+<div className="sidebar-buttons">
 <Link to="/" onClick = {() => {window.location.href="/"}} > Home </Link>
 <Link to="/Login" onClick = {() => {window.location.href="/Login"}} > Login </Link>
 <Link to="SignUp" onClick = {() => {window.location.href="/SignUp"}} > Sign Up </Link>
 <Link to="/CreateEvent" onClick = {() => {window.location.href="/CreateEvent"}} > Create Event </Link>
 <Link to="/VolunteerSignUp" onClick = {() => {window.location.href="/VolunteerSignUp"}} > Volunteer Sign Up </Link>
 <Link to="/Profile" onClick = {() => {window.location.href="/Profile"}} > Profile </Link>
+</div>
 </div>
 </header>
 )
