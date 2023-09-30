38e92eaf2b0722697cb24f5a4ef347ee20d031b2
MsJoyA <ashleyjoyetheridge@gmail.com>
2023-09-30 03:25:13 -0400

    fixed up UI in Javascript

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -65,53 +65,50 @@ function SignUp() {
   }
 
   return (
-    <div className="main_containr">
-      <form className="main_container" onSubmit={handleSubmit}>
-        <label className="input_heading"> First Name </label>
-        <input
-          className="input"
-          type="text"
-          id="firstName"
-          name="firstName"
-          placeholder="Jane"
-          onChange={(e) => setFirstName(e.target.value)}
-        ></input>
+    <div className='main_containr'>
+    <div className='header_bar'>
+      <img src={logo} className='logo'></img>
+      <h1 className='heading'> Create An Account </h1>
+    </div>
+    
+     <form className='main_container' onSubmit={handleSubmit}>
+      
+      <div className='name_entry'>
+        <div className='vertical_name_stack'>
+          <label className='input_heading'> First Name </label>
+          <input className='input' type='text' 
+            id='firstName' name='firstName' placeholder='Jane'
+            onChange={(e) => setFirstName(e.target.value)}></input>
+        </div>
+        <div className='vertical_name_stack'>
+          <label className='input_heading'> Last Name </label>
+          <input className='input' type='text' 
+          name='lastName' placeholder='Jane'
+          onChange={(e) => setLastName(e.target.value)}></input>
+        </div>
+      </div>
 
-        <label className="input_heading"> Last Name </label>
-        <input
-          className="input"
-          type="text"
-          name="lastName"
-          placeholder="Jane"
-          onChange={(e) => setLastName(e.target.value)}
-        ></input>
-
-        <label className="input_heading"> Email </label>
-        <input
-          className="input"
-          id="email"
-          name="email"
-          placeholder="enter email"
-          onChange={(e) => setEmail(e.target.value)}
-          type="email"
-        ></input>
-
-        <label className="input_heading"> Password </label>
-        <input
-          className="input"
-          type="password"
-          id="password"
-          name="password"
-          placeholder="enter password"
-          onChange={(e) => setPassword(e.target.value)}
-        ></input>
-
-        <label className="input_heading"> Host? </label>
-        <input
-          className="input"
-          type="checkbox"
-          onChange={() => setIsHost((prev) => !prev)}
-        ></input>
+      <div className='name_entry'>
+        <div className='vertical_name_stack'>
+          <label className='input_heading'> Email </label>
+          <input className='input'id='email' name='email' placeholder='enter email'
+          onChange={(e) => setEmail(e.target.value)} type="email"></input>
+        </div>
+        <div className='vertical_name_stack'>
+          <label className='input_heading'> Password </label>
+          <input className='input' type='password' 
+            id='password' name='password' placeholder='enter password'
+            onChange={(e) => setPassword(e.target.value)}></input>
+        </div>
+      </div >
+      
+      <div className='name_entry'>
+      <label className='input_heading'> Check if you are a host:  </label>
+      <input className='input' type='checkbox' 
+          onChange={
+            (e) => setIsHost(e.target.value)}></input>
+        
+      </div>
 
         <div className="horizontal_stack">
           <input
