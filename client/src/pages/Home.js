39861573ddd5943bc99bf004bd846d5be9d1c39c
MsJoyA <ashleyjoyetheridge@gmail.com>
2023-09-30 01:39:35 -0400

    Started planning for recommendation system

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,7 +9,39 @@ function Home() {
   const [totalVolunteerHours, setTotalVolunteerHours] = useState(0);
   const [totalEvents, setTotalEvents] = useState(50000);
   const [totalUsers, setTotalUsers] = useState(2);
+  const [recommendedEvents, setRecommendedEvents] = useState([]);
+  // const queryRecommendedEvents = JSON.stringify(
+  //   {
+  //       query1: 
+  //       query2:
+  //       query3:
 
+
+  //   }
+  // );
+  
+
+
+  // get recommended user events
+  useEffect(() => {
+    fetch('api/recommend-events').then((response) => response.json())
+    .then((data) => {
+      setRecommendedEvents(data.events)
+
+      // generates random range of 10 events to be presented
+      // or if length is less than 10, then all recommended
+      // events are presented
+      if (recommendedEvents.length > 5){
+        startingValue = Math.random(0, recommendedEvents.length-5)
+        setRecommendedEvents(recommendedEvents.slice(startingValue, startingValue + 5))
+      }
+    }).catch((error) => {
+      console.error(error)
+    })
+  })
+
+
+  
   useEffect(() => {
     // Fetch the total volunteer hours 
     fetch('/api/volunteer-hours')
