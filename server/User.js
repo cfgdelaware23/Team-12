88ae2c4cea04ff2a16887f3951a62117850d8d95
Mauricio Del Castillo <mauriciodelcas30@gmail.com>
2023-09-30 01:33:15 -0400

    connection to backend successful

--- server/User.js
+++ server/User.js
@@ -2,12 +2,11 @@
 
 const mongoose = require("mongoose");
 
-const userSchema = new mongoos.schema({
+const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  userType: { type: Number, required: true },
   isHost: { type: Boolean, required: true },
   totalHours: { type: Number, required: true },
 });
